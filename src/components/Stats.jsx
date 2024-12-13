d8c233810224a93c02ecaa0df7b98553ebcc0447
niloykhan002 <barkatificbankbd@gmail.com>
2024-12-13 21:49:13 +0600

    statistics section added

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,5 +1,31 @@
 const Stats = () => {
-  return <div className="flex justify-center items-center my-28"></div>;
+  return (
+    <div className="flex justify-center items-center my-28">
+      <div>
+        <h1 className="text-4xl font-bold text-center">Statistics</h1>
+        <div className="divider"></div>
+        <div className="stats shadow-lg mt-12">
+          <div className="stat place-items-center p-28">
+            <div className="stat-title">Total Donations</div>
+            <div className="stat-value">31K</div>
+            <div className="stat-desc">From January 1st to December 1st</div>
+          </div>
+
+          <div className="stat place-items-center p-28">
+            <div className="stat-title">Users</div>
+            <div className="stat-value text-secondary">4,200</div>
+            <div className="stat-desc text-secondary">↗︎ 40 (2%)</div>
+          </div>
+
+          <div className="stat place-items-center p-28">
+            <div className="stat-title">New Registers</div>
+            <div className="stat-value">1,200</div>
+            <div className="stat-desc">↘︎ 90 (14%)</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Stats;
